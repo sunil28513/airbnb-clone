2032601bf7e0de5a96fbd13f886ad43202f4a2e1
Verma <v.nishad@slnsoftwares.com>
2020-11-22 10:48:41 +0530

    Updates

---
+++ precache-manifest.e41884e19b7e84a91b584c2073287c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc736ae6941125a86ef108460891fa77",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "5f1d4681d2bf92a7c54a",
+    "url": "/airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "78dbbd494810cfdba696",
+    "url": "/airbnb-clone/static/css/main.95d250fe.chunk.css"
+  },
+  {
+    "revision": "5f1d4681d2bf92a7c54a",
+    "url": "/airbnb-clone/static/js/2.1bd9d185.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.1bd9d185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78dbbd494810cfdba696",
+    "url": "/airbnb-clone/static/js/main.7c0e67a4.chunk.js"
+  },
+  {
+    "revision": "e6989ffff8373d77b042",
+    "url": "/airbnb-clone/static/js/runtime-main.341e2210.js"
+  }
+]);
